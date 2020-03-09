01dfdb604b159960339fd7be8bab12ab664d8745
Ganesh Chittella <madhavi.0108@gmail.com>
2020-03-08 20:12:09 -0700

    created folder structure to follow MVC design pattern. Added schema.sql and seeds.sql file to create database and seed it with burgers data to insert into burgers table

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name ,devoured ) VALUES ('Vegan BBQ Tofu Burger',false);
+INSERT INTO burgers (burger_name ,devoured ) VALUES ('Crispy Quinoa Burger',true);
+INSERT INTO burgers (burger_name ,devoured ) VALUES ('Portobello and Peach Burger',false);
