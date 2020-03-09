3abfa181a7f0733edacb5c2bd2e5962e40d25714
Ganesh Chittella <madhavi.0108@gmail.com>
2020-03-08 23:53:25 -0700

    created the code that will call the ORM functions using burger specific input for the ORM.

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require('../config/orm');
+
+var burger = {
+    selectAll : function(cb){
+        orm.selectAll("burgers",function(result){
+            cb(result);
+        });
+    },
+    insertOne : function(cols,vals,cb){
+        orm.insertOne("burgers",cols,vals,function(result){
+            cb(res);
+        });
+    },
+    updateOne : function(colValObj,condition,cb){
+        orm.updateOne("burgers",colValObj,condition,function(result){
+            cb(result);
+        });
+    }
+};
+
+module.exports = burger;
